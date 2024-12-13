6a457205c460442bfc5a76da725d4dd06eac6436
Underquartz <underquartz@gmail.com>
2024-12-13 12:03:29 -0300

    Primeiras janelas do LDS

---
+++ ProjetoPadaria/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ProjetoPadaria
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(660, 159);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 48);
+            label1.TabIndex = 0;
+            label1.Text = "Nome";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(825, 156);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(396, 55);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(825, 307);
+            textBox2.Name = "textBox2";
+            textBox2.PasswordChar = '*';
+            textBox2.Size = new Size(396, 55);
+            textBox2.TabIndex = 2;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(660, 307);
+            label2.Name = "label2";
+            label2.Size = new Size(126, 48);
+            label2.TabIndex = 3;
+            label2.Text = "Senha";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(660, 446);
+            button1.Name = "button1";
+            button1.Size = new Size(273, 125);
+            button1.TabIndex = 4;
+            button1.Text = "LOGIN";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(990, 446);
+            button2.Name = "button2";
+            button2.Size = new Size(249, 125);
+            button2.TabIndex = 5;
+            button2.Text = "CADASTRO";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Black", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(495, 29);
+            label3.Name = "label3";
+            label3.Size = new Size(211, 74);
+            label3.TabIndex = 6;
+            label3.Text = "LOGIN";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(90, 127);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(522, 462);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(20F, 48F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1295, 705);
+            Controls.Add(pictureBox1);
+            Controls.Add(label3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Tela Login";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private Label label3;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ ProjetoPadaria/Form2.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ProjetoPadaria
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label3 = new Label();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(98, 158);
+            label1.Name = "label1";
+            label1.Size = new Size(119, 48);
+            label1.TabIndex = 0;
+            label1.Text = "Login";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(76, 299);
+            label2.Name = "label2";
+            label2.Size = new Size(190, 48);
+            label2.TabIndex = 1;
+            label2.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(272, 155);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(849, 55);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(272, 292);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(852, 55);
+            textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(272, 410);
+            button1.Name = "button1";
+            button1.Size = new Size(393, 165);
+            button1.TabIndex = 4;
+            button1.Text = "CADASTRAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Black", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(419, 35);
+            label3.Name = "label3";
+            label3.Size = new Size(342, 74);
+            label3.TabIndex = 5;
+            label3.Text = "CADASTRO";
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(728, 410);
+            button2.Name = "button2";
+            button2.Size = new Size(396, 165);
+            button2.TabIndex = 6;
+            button2.Text = "TELA LOGIN";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(20F, 48F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1235, 705);
+            Controls.Add(button2);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Tela Cadastro";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Label label3;
+        private Button button2;
+    }
+}
